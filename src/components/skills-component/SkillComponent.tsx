f824fcb7c3f4e4ad4a1c1dfb5dd15da4a92ff786
AlonaRahilevych <ragilevic@gmail.com>
2026-02-03 21:57:44 +0100

    style:update styles section

--- src/components/skills-component/SkillComponent.tsx
+++ src/components/skills-component/SkillComponent.tsx
@@ -4,8 +4,8 @@ import styles from './SkillComponent.module.css';
 export const SkillComponent = ({ skill }: { skill: ISkill }) => {
   return (
     <div className={styles.component}>
+      <p>{skill.category}</p>
       <ul className={styles.list}>
-        <li>{skill.category}</li>
         {skill.items.map((item) => (
           <li key={item}>{item}</li>
         ))}
--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,4 +1,6 @@
 'use client';
+import '../../i18n/i18n';
+import { useTranslation } from 'react-i18next';
 import { skills } from '@/data/skillsData';
 import { SectionTitle } from '../section-title/SectionTitle';
 import styles from './Skills.module.css';
@@ -6,6 +8,7 @@ import { motion } from 'framer-motion';
 import { SkillComponent } from '../skills-component/SkillComponent';
 
 export const Skills = () => {
+  const { t } = useTranslation();
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -13,8 +16,9 @@ export const Skills = () => {
         opacity: 1,
         transition: { delay: 1.5, duration: 0.8, ease: 'easeInOut' },
       }}
+      className={`section ${styles.section}`}
     >
-      <SectionTitle>Skills</SectionTitle>
+      <SectionTitle>{t('skills.title')}</SectionTitle>
       <div className={styles.skills}>
         {skills.map((skill) => (
           <SkillComponent key={skill.category} skill={skill} />
